fa605fef4a7eb9a94dc649d0d1dc8511edd3e828
Sohayyy <sohamoosa153@gmail.com>
2018-06-22 23:13:43 +0500

    Authentication Firebase

---
+++ auth/login.js
@@ -0,0 +1,61 @@
+// // Initialize Firebase
+
+var config = {
+    apiKey: "AIzaSyBUvXdnEULdtBJCRMfFM_mWOQNdDlD-g34",
+    authDomain: "patient-tracker-app-8c634.firebaseapp.com",
+    databaseURL: "https://patient-tracker-app-8c634.firebaseio.com",
+    projectId: "patient-tracker-app-8c634",
+    storageBucket: "",
+    messagingSenderId: "934358866547"
+};
+
+var firebase = firebase.initializeApp(config);
+
+
+// login function
+var auth = firebase.auth();
+
+
+document.getElementById("loginForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    console.log(email, password);
+
+    auth.signInWithEmailAndPassword(email,password)
+        .then(function(data){
+//             console.log("Response", data); 
+//         // window.location.href = "../dashboard.html";
+
+//         // local storage
+
+//         localStorage.setItem("user", JSON.stringify(data.user));
+
+//         // get data
+
+//         let user = localStorage.getItem("user");
+//         user = JSON.parse(user);
+//         console.log("User Object Data: " , user);
+
+// //        remove data
+//         setTimeout(  ()=>{
+//             localStorage.removeItem("user");
+//         }, 4000); 
+
+         localStorage.setItem("user", JSON.stringify(data.user));
+            
+        })
+        
+        .catch(function(error){
+            console.log(error.message);
+        })
+
+    
+        });
+
+auth.onAuthStateChanged(function(user){
+    if(user){
+        window.location.href = "../dashboard.html";
+
+    }
+});
---
+++ auth/signin.js
@@ -0,0 +1,61 @@
+
+// // Initialize Firebase
+
+var config = {
+    apiKey: "AIzaSyBUvXdnEULdtBJCRMfFM_mWOQNdDlD-g34",
+    authDomain: "patient-tracker-app-8c634.firebaseapp.com",
+    databaseURL: "https://patient-tracker-app-8c634.firebaseio.com",
+    projectId: "patient-tracker-app-8c634",
+    storageBucket: "",
+    messagingSenderId: "934358866547"
+};
+
+var firebase = firebase.initializeApp(config);
+
+console.log("Firebase Running");
+console.log("Details", firebase);
+
+
+// signup function
+
+document.getElementById("signupForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    let firstName = document.getElementById("firstName").value;
+    let lastName = document.getElementById("lastName").value;
+    let user = document.getElementById("username").value;
+    let email = document.getElementById("email").value;
+    let age = document.getElementById("age").value;
+    let radioButtonValue = undefined;
+    let buttons = document.getElementById("gender");
+    for (let i = 0; i < buttons.length; i++) {
+        if (buttons[i].checked == true) {
+            radioButtonValue = buttons[i];
+        }
+    }
+    let dateOfBirth = document.getElementById("dateOfBirth").value;
+    let password = document.getElementById("password").value;
+    let confirmPassword = document.getElementById("confirmPassword").value;
+
+
+
+    let auth =    firebase.auth();
+
+    auth.createUserWithEmailAndPassword(email,password)
+        .then( function(){
+            console.log("Account Created!");
+            window.location.href = "../dashboard.html"
+        }).catch(function(error){
+            console.log("Error: ", error);
+        })
+    
+
+});
+
+firebase.auth().onAuthStateChanged( function(user){
+    if(user){
+        window.location.href = "../dashboard.html";
+    }
+    else{
+        console.log("Logged Out!");
+    }
+});
